b4dc58298384da07ff4b74433645652cfc0109b6
huhuics <hhuics@163.com>
2017-08-25 20:54:42 +0800

    测试任务添加

---
+++ joice-service/src/main/java/org/joice/service/support/scheduler/job/SayHi.java
@@ -0,0 +1,21 @@
+/**
+ * 深圳金融电子结算中心
+ * Copyright (c) 1995-2017 All Rights Reserved.
+ */
+package org.joice.service.support.scheduler.job;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 
+ * @author HuHui
+ * @version $Id: SayHi.java, v 0.1 2017年8月25日 下午8:41:34 HuHui Exp $
+ */
+@Component("sayHi")
+public class SayHi {
+
+    public void say() {
+        System.out.println(" Hi from SayHi! ");
+    }
+
+}
