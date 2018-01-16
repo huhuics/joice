3ef4d7ad853549f7a6905a2c7452d896f951e05a
huhuics <hhuics@163.com>
2018-01-16 10:17:51 +0800

    增加joice-schedule工程

---
+++ joice-schedule/src/main/java/org/joice/schedule/App.java
@@ -0,0 +1,13 @@
+package org.joice.schedule;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
